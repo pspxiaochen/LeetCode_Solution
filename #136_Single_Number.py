3312885a309a79932762f698df7a2c6f31c8154b
xiaochen <476304074@qq.com>
2017-09-27 10:41:00 +0800

    add 136

---
+++ #136_Single_Number.py
@@ -0,0 +1,22 @@
+def singleNumber_1(nums):
+    if len(nums) == 1:
+        return nums[0]
+    dict ={}
+    for i in nums:
+        if i not in dict:
+            dict[i] = 1
+        else:
+            dict[i] += 1
+    for key,value in dict.items():
+        if value == 1:
+            return key
+
+#
+def singleNumber_2(nums):
+    result = 0
+    for i in nums:
+        result = result ^ i
+    return result
+
+def singleNumber_3(nums):
+    return 2*sum(set(nums)) - sum(nums)

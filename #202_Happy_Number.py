2f865d115a40320f0e08a51513038b62860fbaf4
xiaochen <476304074@qq.com>
2017-10-26 11:19:40 +0800

    add 202

--- #202_Happy_Number.py
+++ #202_Happy_Number.py
@@ -1,4 +1,4 @@
-def isHappy(n):
+def isHappy_1(n):
     list = []
     b = n
     while n > 0:
@@ -12,6 +12,30 @@ def isHappy(n):
            list.append(b)
         else:
             return False
+def isHappy_2(n):
+    list = []
+    while n not in list:
+        list.append(n)
+        squareSum = 0
+        while n > 0:
+            remain = n % 10
+            squareSum += remain * remain
+            n //= 10
+        if squareSum == 1:
+            return True
+        else:
+            n = squareSum
+    return False
 
-print(isHappy(1))
+def isHappy_3(n):
+    mem = set()
+    while n != 1:
+        n = sum([int(i) **2 for i in str(n)])
+        if n in mem:
+            return False
+        else:
+            mem.add(n)
+    return True
+
+
 

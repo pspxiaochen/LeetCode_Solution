2e5b9ae89eefe51515ef400c4dcedb40f290df51
xiaochen <476304074@qq.com>
2017-11-13 10:01:04 +0800

    add 234

--- #234_Palindrome_Linked_List.py
+++ #234_Palindrome_Linked_List.py
@@ -14,4 +14,17 @@ def isPalindrome_1(head):
     return list == list[::-1]
 
 def isPalindrome_2(head):
-
+    rev = None
+    slow = fast = head
+    while fast and fast.next:
+        fast = fast.next.next
+        temp = rev
+        rev = slow
+        slow = slow.next
+        rev.next = temp
+    if fast:
+        slow = slow.next
+    while rev and rev.val == slow.val:
+        rev = rev.next
+        slow = slow.next
+    return not rev

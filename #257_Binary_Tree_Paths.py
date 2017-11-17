4e5df8c12d75443956b3632a0ffa6f566953fcae
xiaochen <476304074@qq.com>
2017-11-17 16:11:43 +0800

    add 257

--- #257_Binary_Tree_Paths.py
+++ #257_Binary_Tree_Paths.py
@@ -5,4 +5,30 @@ class TreeNode(object):
         self.right = None
 
 def binaryTreePath(root):
-    
+    paths = []
+    if not root:
+        return paths
+    getPath(root,'',paths)
+    return paths
+
+def getPath(root,path,paths):
+    if root.left == None and root.right == None:
+        paths.append(path+str(root.val))
+    if root.left != None:
+        getPath(root.left,path + str(root.val) + '->',paths)
+    if root.right != None:
+        getPath(root.right,path + str(root.val) + '->' ,paths)
+
+def binaryTreePath_2(root):
+    if not root:
+        return []
+    paths,stack = [],[(root,"")]
+    while len(stack) != 0:
+        node,path = stack.pop()
+        if not node.left and not node.right:
+            paths.append(path + str(node.val))
+        if node.left:
+            stack.append((node.left,path + str(node.val) + '->'))
+        if node.right:
+            stack.append((node.right, path + str(node.val) + '->'))
+    return paths

07b65736e3b8bcd76fac20d14189b1583fd495e3
xiaochenpsp <pspxiaochen@gmail.com>
2018-03-16 17:51:19 +0800

    add 669

--- #669_Trim_a_Binary_Search_Tree.py
+++ #669_Trim_a_Binary_Search_Tree.py
@@ -4,6 +4,7 @@ class TreeNode:
         self.left = None
         self.right = None
 
+# 递归写法
 def trimBST(root,L,R):
     if not root:
         return root
@@ -15,3 +16,27 @@ def trimBST(root,L,R):
         root.left = trimBST(root.left,L,R)
         root.right = trimBST(root.right,L,R)
         return root
+
+# 非递归写法
+
+def trimBST_2(root,L,R):
+    if not root:
+        return root
+    while root.val < L or root.val > R:
+        if root.val < L:
+            root = root.right
+        if root.val > R:
+            root = root.left
+
+    dummy = root
+    while dummy:
+        while dummy.left and dummy.left.val < L:
+            dummy.left = dummy.left.right
+        dummy = dummy.left
+
+    dummy = root
+    while dummy:
+        while dummy.right and dummy.right.val > R:
+            dummy.right = dummy.right .left
+        dummy = dummy.right
+    return root

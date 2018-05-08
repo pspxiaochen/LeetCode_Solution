ec32d6cc9a5468ae6e83951d976f632a51234fa7
xiaochenpsp <pspxiaochen@gmail.com>
2018-05-08 14:05:11 +0800

    add 8

---
+++ #8_String_to_Integer(atoi).py
@@ -0,0 +1,14 @@
+from itertools import takewhile
+def myAtoi(self, s):
+    s = s.lstrip()
+
+    sign = list(takewhile(lambda ch: ch in set("+-"), s))
+    if len(sign) > 1:
+        return 0
+
+    digits = list(takewhile(lambda ch: ch in set("1234567890"), s[len(sign):]))
+    try:
+        r = int("".join(sign) + "".join(digits))
+    except ValueError:
+        return 0
+    return max(min(r, 2 ** 31 - 1), -2 ** 31)

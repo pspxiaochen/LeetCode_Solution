eaa7d843c9ff01fe108decc42fba1e4fb769e032
xiaochenpsp <pspxiaochen@gmail.com>
2018-05-14 23:02:35 +0800

    add 22

---
+++ 22_Generate_Parentheses.py
@@ -0,0 +1,14 @@
+def generateParenthesis(n):
+    res= []
+    help(n,n,'',res)
+    print( res)
+
+def help(left,right,item,res):
+    if right < left:
+        return
+    if left == 0 and right == 0:
+        res.append(item)
+    if left > 0:
+        help(left - 1,right,item + "(",res)
+    if right > 0 :
+        help(left,right - 1,item + ')',res)
